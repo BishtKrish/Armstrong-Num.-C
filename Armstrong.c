4b471ad0a01238c923dabb0e3f6693ef9cdeb942
Krishna Singh Bisht <141957819+BishtKrish@users.noreply.github.com>
2023-08-30 11:44:47 +0530

    Add files via upload

---
+++ Armstrong.c
@@ -0,0 +1,19 @@
+#include<stdio.h>  
+ int main()    
+{    
+int n,r,sum=0,a;    
+printf("enter the number=");    
+scanf("%d",&n);    
+a =n;    
+while(n>0)    
+{    
+r=n%10;    
+sum=sum+(r*r*r);    
+n=n/10;    
+}    
+if(a==sum)    
+printf("armstrong  number ");    
+else    
+printf("not armstrong number");    
+return 0;  
+}   
